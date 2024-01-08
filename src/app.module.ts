28221a11ca893ba2e69b19e592b268d26b9560a2
GwFra <george.franklin66@gmail.com>
2024-01-08 12:14:52 +0000

    feat(endpoints): Nest generated bids controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BidsModule } from './bids/bids.module';
 
 @Module({
-  imports: [],
+  imports: [BidsModule],
   controllers: [AppController],
   providers: [AppService],
 })
