57252bed27c207003ec082e602cbcbad00de3ede
GwFra <george.franklin66@gmail.com>
2024-01-08 22:41:06 +0000

    feat(setup): Added service boilerplate for bids

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BidsModule } from './bids/bids.module';
+import { BidsController } from './bids/bids.controller';
+import { BidsService } from './bids/bids.service';
 
 @Module({
   imports: [BidsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BidsController],
+  providers: [AppService, BidsService],
 })
 export class AppModule {}
