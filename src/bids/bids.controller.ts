2629666037830e89df415cddf9b1d404f44b1c76
GwFra <george.franklin66@gmail.com>
2024-01-09 08:40:44 +0000

    feat(bids): Created initial bids endpoints for fetching only

--- src/bids/bids.controller.ts
+++ src/bids/bids.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { BidsService } from './bids.service';
 
 @Controller('bids')
 export class BidsController {
   constructor(private readonly bidsService: BidsService) {}
-}
+
+  @Get()
+  allBids(): object {
+    return this.bidsService.getBids();
+  }
+
+  @Get(":id")
+  singleBid(@Param("id") searchId): object {
+    return this.bidsService.getSingleBid(searchId);
+  }
+
+  @Get("history/:id")
+  singleBidHistory(@Param("id") searchId): object {
+    return this.bidsService.getBidHistory(searchId);
+  }
+  }
--- src/bids/bids.service.ts
+++ src/bids/bids.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BidsService {}
+export class BidsService {
+
+    private bids = [
+        { id: 1, owner: "", start: 111}
+    ]
+
+    private bidsHistory = [
+        { id: 1, history: []},
+        { id: 2, history: []},
+        { id: 3, history: []},
+    ]
+
+    findBid(searchId, isHistorical = false) {
+        const searchObj =  isHistorical ? this.bids : this.bidsHistory;
+        return searchObj.find(({id}) => id === searchId)
+    }
+
+    getBids() {
+        return this.bids
+    }
+
+    getSingleBid(searchId: number) {
+        return this.findBid(searchId);
+    }
+
+    getBidHistory(searchId: number) {
+        return this.findBid(searchId, true);
+    }
+
+}
