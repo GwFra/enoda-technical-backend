9a0eeb38a2000ee67f2664910e2b749a1ea5b9a6
GwFra <george.franklin66@gmail.com>
2024-01-10 22:31:29 +0000

    fix(auth): Removed google secret and id from repo

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 // import { In}
 import { Strategy, VerifyCallback } from 'passport-google-oauth2';
+import 'dotenv/config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID:
-        '807452420141-r11jc2hdpsrv2tqcrlfvjgsghm4uac8b.apps.googleusercontent.com',
-      clientSecret: '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',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/auth/google/callback',
       scope: ['profile', 'email'],
     });
